3437c4269fefe44ab7ad63d3e9617f7a9dc8d299
teja suryavamshi <chandramahesh93900@gmail.com>
2026-05-08 15:22:55 +0530

    fixed a java services files

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,12 +2,10 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.service.OrderAggregationService;
 import java.util.Map;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
 public class OrderController {
 
     private final OrderAggregationService orderAggregationService;
